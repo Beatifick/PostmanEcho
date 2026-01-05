c96d9fcb7d85b3aad5901ae9e48acdeedd141191
Beatific <1telpenkov1@mail.ru>
2026-01-05 14:03:43 +0300

    main

---
+++ src/test/java/ru/netology/rest/PostmanEchoTest.java
@@ -0,0 +1,24 @@
+package ru.netology.rest;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+class PostmanEchoTest {
+
+    @Test
+    void testPostBody() {
+        String requestBody = "some data";
+
+        given()
+                .baseUri("https://postman-echo.com")
+                .contentType("text/plain; charset=UTF-8")
+                .body(requestBody)
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("data", equalTo(requestBody));
+    }
+}
